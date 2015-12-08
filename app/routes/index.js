69f28b2d518a2c892f20b42632d04a497adf5c69
Nick Hu <nick@nickhu.info>
2015-12-08 11:03:13 +0800

    local passport

--- app/routes/index.js
+++ app/routes/index.js
@@ -66,5 +66,18 @@ module.exports = function (app, passport) {
 		    res.redirect('/');
 		  });
 
+		app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+		}));
+
+		app.post('/login',
+		passport.authenticate('local-login'),
+		function(req, res) {
+		res.redirect('/' + req.user);
+		});
+
+
 
 };
