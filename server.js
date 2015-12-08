6983fba5cacabdaeb9e9ba400795cc04fa6d3b36
Nick Hu <nick@nickhu.info>
2015-12-08 09:54:25 +0800

    dependencies

--- server.js
+++ server.js
@@ -2,34 +2,46 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var flash = require('connect-flash');
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+
 
 var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
+app.use(bodyParser.json({}));
+
 mongoose.connect(process.env.MONGO_URI);
 
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+	app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-	app.use('/public', express.static(process.cwd() + '/public'));
 
-	app.use(session({
-		secret: 'secretClementine',
-		resave: false,
-		saveUninitialized: true
-	}));
+app.use(session({
+	secret: 'secretClementine',
+	resave: false,
+	saveUninitialized: true
+}));
 
-	app.use(passport.initialize());
-	app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-	routes(app, passport);
+routes(app, passport);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
-
-
