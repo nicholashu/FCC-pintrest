ce29bea0209aa6d0ef8f52e5138ec471b25783f1
Nick Hu <nick@nickhu.info>
2015-12-08 10:52:45 +0800

    twitter function

--- app/config/passport.js
+++ app/config/passport.js
@@ -22,21 +22,9 @@ module.exports = function (passport) {
     consumerSecret: configAuth.twitterAuth.consumerSecret,
     callbackURL: configAuth.githubAuth.callbackURL
   },
-  function(token, tokenSecret, profile, done) {
-    User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
-
-	passport.use(new GitHubStrategy({
-		clientID: configAuth.githubAuth.clientID,
-		clientSecret: configAuth.githubAuth.clientSecret,
-		callbackURL: configAuth.githubAuth.callbackURL
-	},
 	function (token, refreshToken, profile, done) {
 		process.nextTick(function () {
-			User.findOne({ 'github.id': profile.id }, function (err, user) {
+			User.findOne({ 'twitter.id': profile.id }, function (err, user) {
 				if (err) {
 					return done(err);
 				}
@@ -46,12 +34,10 @@ module.exports = function (passport) {
 				} else {
 					var newUser = new User();
 
-					newUser.github.id = profile.id;
-					newUser.github.username = profile.username;
-					newUser.github.displayName = profile.displayName;
-					newUser.github.publicRepos = profile._json.public_repos;
-					newUser.nbrClicks.clicks = 0;
-
+					newUser.twitter.id = profile.id;
+					newUser.twitter.token = profile.token;
+					newUser.twitter.displayName = profile.displayName;
+					newUser.twitter.username = profile.username;
 					newUser.save(function (err) {
 						if (err) {
 							throw err;
@@ -63,4 +49,139 @@ module.exports = function (passport) {
 			});
 		});
 	}));
+
+
+passport.use(new GitHubStrategy({
+	clientID: configAuth.githubAuth.clientID,
+	clientSecret: configAuth.githubAuth.clientSecret,
+	callbackURL: configAuth.githubAuth.callbackURL
+},
+function (token, refreshToken, profile, done) {
+	process.nextTick(function () {
+		User.findOne({ 'github.id': profile.id }, function (err, user) {
+			if (err) {
+				return done(err);
+			}
+
+			if (user) {
+				return done(null, user);
+			} else {
+				var newUser = new User();
+
+				newUser.github.id = profile.id;
+				newUser.github.username = profile.username;
+				newUser.github.displayName = profile.displayName;
+				newUser.github.publicRepos = profile._json.public_repos;
+				newUser.save(function (err) {
+					if (err) {
+						throw err;
+					}
+
+					return done(null, newUser);
+				});
+			}
+		});
+	});
+}));
+
+	passport.use('local-signup', new LocalStrategy({
+			// by default, local strategy uses username and password, we will override with email
+			usernameField : 'email',
+			passwordField : 'password',
+			passReqToCallback : true // allows us to pass back the entire request to the callback
+	},
+	function(req, email, password, done) {
+
+			// asynchronous
+			// User.findOne wont fire unless data is sent back
+			process.nextTick(function() {
+
+			// find a user whose email is the same as the forms email
+			// we are checking to see if the user trying to login already exists
+			User.findOne({ 'local.email' :  email }, function(err, user) {
+					// if there are any errors, return the error
+					if (err)
+
+							return done(err);
+
+					// check to see if theres already a user with that email
+					if (user) {
+							return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+					} else {
+
+							// if there is no user with that email
+							// create the user
+							var newUser            = new User();
+
+							// set the user's local credentials
+							newUser.local.email    = email;
+							newUser.local.password = newUser.generateHash(password);
+
+							// save the user
+							newUser.save(function(err) {
+									if (err)
+											throw err;
+									return done(null, newUser);
+							});
+					}
+
+			});
+
+			});
+
+	}));
+
+passport.use('local-login', new LocalStrategy({
+			// by default, local strategy uses username and password, we will override with email
+			usernameField : 'email',
+			passwordField : 'password',
+			passReqToCallback : true // allows us to pass back the entire request to the callback
+	},
+	function(req, email, password, done) { // callback with email and password from our form
+			// find a user whose email is the same as the forms email
+			// we are checking to see if the user trying to login already exists
+			User.findOne({ 'local.email' :  email }, function(err, user) {
+					// if there are any errors, return the error before anything else
+					if (err)
+							return done(err);
+
+					// if no user is found, return the message
+					if (!user)
+							return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+
+					// if the user is found but the password is wrong
+					if (!user.validPassword(password))
+							return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+
+					// all is well, return successful user
+					return done(null, user);
+			});
+
+	}));
+
+passport.use('local-update', new LocalStrategy({
+	usernameField : 'username',
+	passReqToCallback : true
+},
+function(req, username, done) {
+			Users.findOneAndUpdate({
+			"_id": req.body.id
+			}, {
+			local: {
+			email : req.body.email,
+					},
+					 shared: {
+							name: req.body.name,
+							country: req.body.country,
+					state: req.body.state,
+					city: req.body.city
+					 }},
+					 function(err) {
+					if (err) {
+							throw err;
+					}
+					res.send(req.body);
+			});
+
+}));
 };
