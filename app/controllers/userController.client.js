7d7c98d6c5dc3681d9468b21d6e47d9dfa0d15c9
Nick Hu <nick@nickhu.info>
2015-12-08 11:09:41 +0800

    local

---
+++ app/controllers/userController.client.js
@@ -0,0 +1,72 @@
+'use strict';
+
+(function() {
+    angular
+        .module('clementineRecordApp', [])
+        .service('UserService', ['$http', '$window', '$q', function($http, $window, $q) {
+
+        var appUrl = $window.location.origin;
+        var apiUrl = appUrl + '/api/:id';
+
+        var deferred = $q.defer();
+
+        this.getUser = function() {
+            $http.get(apiUrl).then(function(result) {
+                deferred.resolve(result);
+            });
+
+            return deferred.promise;
+        };
+
+
+    }])
+        .controller('UserCtrl', ['$scope', '$http', '$window','UserService','$location',
+
+            function($scope, $http, $window, UserService, $location) {
+                
+                $scope.user = {};
+                $scope.tab = 0;
+                var appUrl = $window.location.origin;
+              //  var userUrl = appUrl + '/api/profile/:id';
+  
+
+                $scope.getUser = function() {
+                UserService.getUser().then(function(result) {
+                        $scope.user = result.data;
+                    });
+                };
+
+                $scope.getUser();
+
+                $scope.stupidButton = function(){
+                	$http.get("/hi").then(function(){
+                		console.log("done");
+                	})
+                };
+
+                $scope.editUser = function(id) {
+                    $http.put( '/api/profile/', {
+                    			'id': $scope.user._id,
+                    			'email':  $scope.user.local.email,
+                    			'password': $scope.user.local.password,
+                        	'name': $scope.user.shared.name,
+													'country': $scope.user.shared.country,
+											   'state': $scope.user.shared.state,
+											    'city': $scope.user.shared.city
+                   }).then(function(user) {
+                  			$window	.location.href = appUrl + "/profile";
+                    });
+                };
+
+
+                $scope.setTab = function(tab) {
+                  $scope.tab = tab;
+                };
+
+                $scope.isTab = function(tab) {
+                  return tab === $scope.tab;
+                };
+            }
+        ]);
+
+})();
---
+++ app/controllers/userHandler.server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+
+var Users = require('../models/user.js');
+var flatten = require('flat')
+
+
+function UserHandler() {
+
+     this.editUser = function(req, res) {
+        Users.findOneAndUpdate({
+        "_id": req.body.id
+        },flatten( {
+        local: {
+        email : req.body.email,
+		    },
+		 shared: {
+			name: req.body.name,
+			country: req.body.country,
+		    state: req.body.state,
+		    city: req.body.city
+		 }}),
+			 function(err) {
+            if (err) {
+                throw err;
+            }
+            res.send(req.body);
+        });
+    };
+
+}
+
+module.exports = UserHandler;
